0afc7f5145b0b1d3cd8055e1c46c07572c5af5fa
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-10-13 00:14:25 +0100

    feat: show empty invoice message

--- lib/screens/invoice_screen.dart
+++ lib/screens/invoice_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class InvoiceScreen extends StatelessWidget {
   const InvoiceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Facture"));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            PhosphorIconsDuotone.invoice,
+            size: 80,
+            color: Colors.grey[400],
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Pas encore de transactions',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Vos factures seront affichées ici après des transactions réussies',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: Colors.grey[500],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
