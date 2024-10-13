fab34d8ca78256f4b4a55eeae42ab5913316e459
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-10-13 14:07:08 +0100

    add conditionnal dispay of download transaction invoice and resubmit button; add invoice url feature

--- lib/screens/invoice_screen.dart
+++ lib/screens/invoice_screen.dart
@@ -1,8 +1,8 @@
 import 'package:afrik_flow/providers/user_notifier.dart';
+import 'package:afrik_flow/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:afrik_flow/models/notification.dart' as notification_model;
 import 'package:go_router/go_router.dart';
 
 class InvoiceScreen extends ConsumerStatefulWidget {
@@ -16,38 +16,107 @@ class _InvoiceScreenState extends ConsumerState<InvoiceScreen> {
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
-    final transactions = user?.transactions;
+    final transactions = user?.transactions ?? [];
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            PhosphorIconsDuotone.invoice,
-            size: 80,
-            color: Colors.grey[400],
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Pas encore de transactions',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey[600],
+    final successfulTransactions = transactions
+        .where((transaction) =>
+            transaction.payoutStatus == "success" &&
+            transaction.invoiceUrl != null)
+        .toList();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: successfulTransactions.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    PhosphorIconsDuotone.invoice,
+                    size: 80,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Pas encore de transactions',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Vos factures seront affichées ici après des transactions réussies',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey[500],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: successfulTransactions.length,
+              itemBuilder: (context, index) {
+                final transaction = successfulTransactions[index];
+                return Card(
+                    color: Colors.black,
+                    elevation: 5,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(
+                            PhosphorIconsDuotone.fileText,
+                            color: Colors.grey[400],
+                            size: 40,
+                          ),
+                          title: Text(
+                            'Vers ${transaction.payoutPhoneNumber}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          subtitle: Text(
+                            'Montant: ${transaction.amount} | ${transaction.humarizeDate}',
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              context.push(transaction.invoiceUrl!);
+                            },
+                            icon: const Icon(
+                              PhosphorIconsDuotone.downloadSimple,
+                              size: 18,
+                            ),
+                            label: const Text('Télécharger'),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppTheme.primaryColor,
+                              foregroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ));
+              },
             ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Vos factures seront affichées ici après des transactions réussies',
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.normal,
-              color: Colors.grey[500],
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
     );
   }
 }
